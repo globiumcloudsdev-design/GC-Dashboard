8cc6f6df86bbba18e1067e17f38e442bca1e2093
Shoaib Raza <hafizshoaibraza180@gmail.com>
2025-12-18 15:55:26 +0500

    Manual Attendance Error

--- src/app/api/attendance/admin/manual/route.js
+++ src/app/api/attendance/admin/manual/route.js
@@ -107,7 +107,6 @@ export async function POST(request) {
       .populate("user", "firstName lastName email")
       .populate("agent", "agentName agentId email")
       .populate("shift", "name startTime endTime")
-      .populate("manager", "firstName lastName email");
 
     return NextResponse.json({ 
       success: true, 
